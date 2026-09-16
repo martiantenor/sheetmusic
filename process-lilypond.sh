bc93524dace942aada8ef00e20194c8169df361c
Dave <blair42@gmail.com>
2026-09-16 16:00:11 -0400

    fix: directory management

--- process-lilypond.sh
+++ process-lilypond.sh
@@ -3,6 +3,18 @@
 ## Style sheet
 stylesheet = "./stylefiles/style.ily"
 
+## Empty folders if they do exist, create folders if they don't
+if [ -d "midi" ]; then
+    rm -rf "midi/*"
+else
+    mkdir "midi"
+fi
+if [ -d "pdf" ]; then
+    rm -rf "midi/*"
+else
+    mkdir "pdf"
+fi
+
 ## Compile loop
 for file in ./source-files/*; do
     lilypond -e '(define-public style-sheet "./stylefiles/style.ily")' "$file"
